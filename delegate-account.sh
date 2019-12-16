9f849dc68c5deca096016075618fdb9712867385
Chris-Graffagnino <graffwebdev@gmail.com>
2019-12-16 11:37:52 -0600

    Update delegate-account.sh for Priyank's most recent changes

--- delegate-account.sh
+++ delegate-account.sh
@@ -12,14 +12,17 @@
 . $(dirname $0)/env
 
 if [ "$1" = "--help" ] || [ $# -lt 2 ]; then
-    echo "usage: $0 <ACCOUNT_SK> <STAKE_POOL_ID>"
+    echo "usage: $0 <ACCOUNT_SK> <STAKE_POOL_IDS><"
     echo "    <ACCOUNT_SK>     The Secret key of the Account address"
-    echo "    <STAKE_POOL_ID>  The Stake pool ID"
+    echo "    <STAKE_POOL_IDS> <ID>:<WEIGHT>"
+    echo ""
+    echo "Multiple pools, separated by spaces, e.g. <ID>:<WEIGHT> <ID>:<WEIGHT>"
+    echo "Weights expressed as integers; script calculates (weight/sum-of-all-weights)"
     exit 1
 fi
 
 ACCOUNT_SK="$1"
-STAKE_POOL_ID="$2"
+STAKE_POOL_ID="${@:2}"
 
 [ -f ${ACCOUNT_SK} ] && ACCOUNT_SK=$(cat ${ACCOUNT_SK})
 
