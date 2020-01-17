124ce5b2be808810be26ac6be0bf689a96cd5a8b
Chris-Graffagnino <graffwebdev@gmail.com>
2020-01-17 14:28:49 -0600

    Add sendmytip.sh for sync to pooltool.io

---
+++ sendmytip.sh
@@ -0,0 +1,27 @@
+!/bin/bash
+
+# USAGE: watch -n 20 '~/files/sendmytip.sh' &> /dev/null &
+
+shopt -s expand_aliases
+RESTAPI_PORT=<YOUR REST_PORT>
+MY_POOL_ID="<YOUR STAKE POOL ID>"
+MY_USER_ID="xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx" # User id from pooltool.io, (see profile), or leave as-is
+THIS_GENESIS="8e4d2a343f3dcf93"   # We only actually look at the first 7 characters
+
+function sendtip() {
+  if [ ! $JORMUNGANDR_RESTAPI_URL ]; then export JORMUNGANDR_RESTAPI_URL=http://127.0.0.1:${RESTAPI_PORT}/api; fi
+  alias cli="$(which jcli) rest v0"
+  local nodestats=$(cli node stats get --output-format json > stats.json);
+
+  local lastBlockHeight=$(cat stats.json | jq -r .lastBlockHeight)
+  local lastBlockHash=$(cat stats.json | jq -r .lastBlockHash)
+  local lastPoolID=$(cli block ${lastBlockHash} get | cut -c169-232)
+
+  if [ "$lastBlockHeight" != "" ]; then
+    local poolToolMax=$(curl -G "https://api.pooltool.io/v0/sharemytip?poolid=${MY_POOL_ID}&userid=${MY_USER_ID}&genesispref=${THIS_GENESIS}&mytip=${lastBlockHeight}&lasthash=${lastBlockHash}&lastpool=${lastPoolID}" | jq -r .pooltoolmax)
+    echo $poolToolMax
+  fi
+}
+
+poolToolHeight=$(sendtip)
+echo $poolToolHeight
