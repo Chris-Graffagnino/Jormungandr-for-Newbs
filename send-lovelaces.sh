6769c4a5d082b853e71a298ccc35844251e7d245
Chris-Graffagnino <graffwebdev@gmail.com>
2020-04-07 12:53:18 -0500

    Update send-lovelaces.sh

--- send-lovelaces.sh
+++ send-lovelaces.sh
@@ -3,83 +3,34 @@
 # Disclaimer:
 #
 #  The following use of shell script is for demonstration and understanding
-#  only, it should *NOT* be used at scale or for any sort of serious
+#  only. It should *NOT* be used at scale or for any sort of serious
 #  deployment, and is solely used for learning how the node and blockchain
 #  works, and how to interact with everything.
 #
-#  It also asumes that `jcli` is in the same folder with the script.
-#  The script works only for Account addresses type.
+#  The script works only for Account address types.
 #
-#  Tutorials can be found here: https://iohk.zendesk.com/hc/en-us/categories/360002383814-Shelley-Networked-Testnet
+#  Tutorials can be found here: https://github.com/input-output-hk/shelley-testnet/wiki
 
-### CONFIGURATION
-CLI="jcli"
-COLORS=1
-ADDRTYPE="--testing"
-TIMEOUT_NO_OF_BLOCKS=200
+. $(dirname $0)/env
 
-getTip() {
-  echo $($CLI rest v0 tip get -h "${REST_URL}")
-}
-
-waitNewBlockCreated() {
-  COUNTER=${TIMEOUT_NO_OF_BLOCKS}
-  echo "  ##Waiting for new block to be created (timeout = $COUNTER blocks = $((${COUNTER} * ${SLOT_DURATION}))s)"
-  initialTip=$(getTip)
-  actualTip=$(getTip)
-
-  while [ "${actualTip}" = "${initialTip}" ]; do
-    sleep ${SLOT_DURATION}
-    actualTip=$(getTip)
-    COUNTER=$((COUNTER - 1))
-    if [ ${COUNTER} -lt 2 ]; then
-      echo "  ##ERROR: Waited $(($COUNTER * $SLOT_DURATION))s secs ($COUNTER*$SLOT_DURATION) and no new block created"
-      exit 1
-    fi
-  done
-  echo "New block was created - $(getTip)"
-}
-
-### COLORS
-if [ ${COLORS} -eq 1 ]; then
-  GREEN=$(printf "\033[0;32m")
-  RED=$(printf "\033[0;31m")
-  BLUE=$(printf "\033[0;33m")
-  WHITE=$(printf "\033[0m")
-else
-  GREEN=""
-  RED=""
-  BLUE=""
-  WHITE=""
-fi
-
-if [ $# -ne 4 ]; then
-  echo "usage: $0 <ADDRESS> <AMOUNT> <REST-LISTEN-PORT> <SOURCE-SK>"
+if [ "$1" = "--help" ] || [ $# -ne 3 ]; then
+  echo "usage: $0 <ACCOUNT_SK> <ADDRESS> <AMOUNT>"
+  echo "    <ACCOUNT_SK>   The Secret key of the Source address"
   echo "    <ADDRESS>     Address where to send the funds"
   echo "    <AMOUNT>      Amount to be sent (in lovelace) - tx fees will be paid by the source address"
-  echo "    <REST-LISTEN-PORT>   The REST Listen Port set in node-config.yaml file (EX: 3101)"
-  echo "    <SOURCE-SK>   The Secret key of the Source address"
   exit 1
 fi
 
-DESTINATION_ADDRESS="$1"
-DESTINATION_AMOUNT="$2"
-REST_PORT="$3"
-SOURCE_SK="$4"
-
-REST_URL="http://127.0.0.1:${REST_PORT}/api"
-BLOCK0_HASH=$($CLI rest v0 settings get -h "${REST_URL}" | grep 'block0Hash:' | sed -e 's/^[[:space:]]*//' | sed -e 's/block0Hash: //')
-FEE_CONSTANT=$($CLI rest v0 settings get -h "${REST_URL}" | grep 'constant:' | sed -e 's/^[[:space:]]*//' | sed -e 's/constant: //')
-FEE_COEFFICIENT=$($CLI rest v0 settings get -h "${REST_URL}" | grep 'coefficient:' | sed -e 's/^[[:space:]]*//' | sed -e 's/coefficient: //')
-MAX_TXS_PER_BLOCK=$($CLI rest v0 settings get -h "${REST_URL}" | grep 'maxTxsPerBlock:' | sed -e 's/^[[:space:]]*//' | sed -e 's/maxTxsPerBlock: //')
-SLOT_DURATION=$($CLI rest v0 settings get -h "${REST_URL}" | grep 'slotDuration:' | sed -e 's/^[[:space:]]*//' | sed -e 's/slotDuration: //')
-SLOTS_PER_EPOCH=$($CLI rest v0 settings get -h "${REST_URL}" | grep 'slotsPerEpoch:' | sed -e 's/^[[:space:]]*//' | sed -e 's/slotsPerEpoch: //')
+ACCOUNT_SK="$1"
+DESTINATION_ADDRESS="$2"
+DESTINATION_AMOUNT="$3"
+[ -f ${ACCOUNT_SK} ] && ACCOUNT_SK=$(cat ${ACCOUNT_SK})
 
 echo "================Send Money================="
+echo "REST_URL: ${JORMUNGANDR_RESTAPI_URL}"
 echo "DESTINATION_ADDRESS: ${DESTINATION_ADDRESS}"
 echo "DESTINATION_AMOUNT: ${DESTINATION_AMOUNT}"
-echo "REST_PORT: ${REST_PORT}"
-echo "SOURCE_SK: ${SOURCE_SK}"
+echo "ACCOUNT_SK: ${ACCOUNT_SK}"
 echo "BLOCK0_HASH: ${BLOCK0_HASH}"
 echo "FEE_CONSTANT: ${FEE_CONSTANT}"
 echo "FEE_COEFFICIENT: ${FEE_COEFFICIENT}"
@@ -95,14 +46,14 @@ fi
 
 set -e
 
-SOURCE_PK=$(echo ${SOURCE_SK} | $CLI key to-public)
+SOURCE_PK=$(echo ${ACCOUNT_SK} | $CLI key to-public)
 SOURCE_ADDR=$($CLI address account ${ADDRTYPE} ${SOURCE_PK})
 
 echo "## Sending ${RED}${DESTINATION_AMOUNT}${WHITE} to ${BLUE}${DESTINATION_ADDRESS}${WHITE}"
 $CLI address info "${DESTINATION_ADDRESS}"
 
 # TODO we should do this in one call to increase the atomicity, but otherwise
-SOURCE_COUNTER=$($CLI rest v0 account get "${SOURCE_ADDR}" -h "${REST_URL}" | grep '^counter:' | sed -e 's/counter: //')
+SOURCE_COUNTER=$($CLI rest v0 account get "${SOURCE_ADDR}" | grep '^counter:' | sed -e 's/counter: //')
 
 # the source account is going to pay for the fee ... so calculate how much
 # FEE_COEFFICIENT should be multiplied witht the no of (INPUTS + OUTPUTS) - we use only 1 Source and 1 Destination
@@ -130,7 +81,7 @@ echo " ##5. Create the witness"
 WITNESS_SECRET_FILE="witness.secret.$$"
 WITNESS_OUTPUT_FILE="witness.out.$$"
 
-printf "${SOURCE_SK}" >${WITNESS_SECRET_FILE}
+printf "${ACCOUNT_SK}" >${WITNESS_SECRET_FILE}
 
 $CLI transaction make-witness ${TRANSACTION_ID} \
   --genesis-block-hash ${BLOCK0_HASH} \
@@ -143,16 +94,17 @@ $CLI transaction add-witness ${WITNESS_OUTPUT_FILE} --staging "${STAGING_FILE}"
 echo " ##7. Show the transaction info"
 $CLI transaction info --fee-constant ${FEE_CONSTANT} --fee-coefficient ${FEE_COEFFICIENT} --staging "${STAGING_FILE}"
 
-echo " ##7. Finalize the transaction and send it"
+echo " ##8. Finalize the transaction and send it"
 $CLI transaction seal --staging "${STAGING_FILE}"
-$CLI transaction to-message --staging "${STAGING_FILE}" | $CLI rest v0 message post -h "${REST_URL}"
+$CLI transaction to-message --staging "${STAGING_FILE}" | $CLI rest v0 message post
 
-echo " ##8. Remove the temporary files"
+echo " ##9. Remove the temporary files"
 rm ${STAGING_FILE} ${WITNESS_SECRET_FILE} ${WITNESS_OUTPUT_FILE}
 
 waitNewBlockCreated
 
-echo " ##9. Check the account's balance"
-$CLI rest v0 account get ${DESTINATION_ADDRESS} -h ${REST_URL}
+echo " ##10. Check the account's balance"
+$CLI rest v0 account get ${DESTINATION_ADDRESS}
 
 exit 0
+ 
